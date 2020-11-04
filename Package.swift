916662f3f8bbcec232fdb7903a066b8593584b51
syaifulQ <syaiful@quadrant.io>
2020-11-05 03:22:43 +0700

    auto solve SPM dependency

--- Package.swift
+++ Package.swift
@@ -10,6 +10,9 @@ let package = Package(
             name: "QDPublisher",
             targets: ["QDPublisher"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/1024jp/GzipSwift.git"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
