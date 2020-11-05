67f0042663cb0b26055bcc68a6bf05755b460359
syaifulQ <70578217+syaifulQ@users.noreply.github.com>
2020-11-05 09:58:55 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "QDPublisher",
-            type: .dynamic,
             targets: ["QDPublisher"]
         )
 
