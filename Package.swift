02d0ff725dab34a73c12f4ced27573355c1d767a
Syaiful Amin <syaiful@quadrant.io>
2020-11-05 07:18:52 +0700

    update auto include dependency for SPM

--- Package.swift
+++ Package.swift
@@ -2,20 +2,22 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
+let target = Target.binaryTarget(name: "QDPublisher", path: "QDPublisher.xcframework")
+target.dependencies = ["Gzip"]
 let package = Package(
-    name: "QDPublisher",
+    name: "QDPublisher Package",
+    platforms: [.iOS(SupportedPlatform.IOSVersion.v8)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "QDPublisher",
-            targets: ["QDPublisher"]),
+            name: "QDPublisher Library",
+            targets: ["QDPublisher"]
+        )
+        
     ],
     dependencies: [
         .package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift.git", from: "5.1.0")
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "QDPublisher", path: "QDPublisher.xcframework")
-    ]
+    targets: [target]
 )
+
